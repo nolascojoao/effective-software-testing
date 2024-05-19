4fde4983302fb26f81c41b0c8fe5dadcd35ed930
João Pedro Nolasco <nolascojoaops@gmail.com>
2024-05-18 22:55:06 -0300

    Ch-01: Write test cases for null, an empty list, and a one-element list

---
+++ ch-01/src/test/java/ch01/PlanningPokerTest.java
@@ -0,0 +1,41 @@
+package ch01;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PlanningPokerTest {
+	
+	@Test
+	void rejectNullInput() {
+		Assertions.assertThatThrownBy(
+				() -> new PlanningPoker().identifyExtremes(null)
+				).isInstanceOf(IllegalArgumentException.class);
+	}
+	// Asserts that an exception happens when we call the method
+	// Asserts that this assertion is an IllegalArgumentException
+	
+	@Test
+	void rejectEmptyList() {
+		Assertions.assertThatThrownBy(() -> {
+			List<Estimate> emptyList = Collections.emptyList();
+			new PlanningPoker().identifyExtremes(emptyList);
+		}).isInstanceOf(IllegalArgumentException.class);
+	}
+	// Similar to the earlier test,ensures that the program 
+	// throws an exception if an empty list of estimates is passed as input
+	
+	@Test
+	void rejectSingleEstimate() {
+		Assertions.assertThatThrownBy(() -> {
+			List<Estimate> list = Arrays.asList(new Estimate("Eleanor", 1));
+			new PlanningPoker().identifyExtremes(list);
+		}).isInstanceOf(IllegalArgumentException.class);
+	}
+	// Ensures that the program throws an exception if a list with a single 
+	// estimate is passed
+
+}
