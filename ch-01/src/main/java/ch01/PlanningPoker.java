fe0264c7779b607dd9ef1113dfd2d690ba6e2e25
João Pedro Nolasco <nolascojoaops@gmail.com>
2024-05-18 22:05:20 -0300

    Ch-01: Add identifyExtremes method to find lowest and highest estimates

---
+++ ch-01/src/main/java/ch01/PlanningPoker.java
@@ -0,0 +1,27 @@
+package ch01;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class PlanningPoker {
+
+	public List<String> identifyExtremes(List<Estimate> estimates) {
+		Estimate lowestEstimate = null;
+		Estimate highestEstimate = null;
+
+		for (Estimate estimate : estimates) {
+			if (highestEstimate == null || 
+					estimate.getEstimate() > highestEstimate.getEstimate()) {
+				highestEstimate = estimate;
+			} else if (lowestEstimate == null || 
+					estimate.getEstimate() < lowestEstimate.getEstimate()) {
+				lowestEstimate = estimate;
+			}
+		}
+
+		return Arrays.asList(
+				lowestEstimate.getDeveloper(), 
+				highestEstimate.getDeveloper()
+				);
+	}
+}
